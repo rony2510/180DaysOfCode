9d18bdfa42df84fee94989b50fb6c42cf50f1d67
rony2510 <ronyhajong2000@gmail.com>
2022-07-07 02:49:45 +0600

    day 7

---
+++ AtCoder Beginner Contest 082/A.cpp
@@ -0,0 +1,45 @@
+/**
+ *    author:  Captcha
+ * 	       RONY HAJONG
+ * 	       CSE'18, SUST
+**/
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false);cin.tie(0);
+#define ll long long
+#define pi acos(-1)
+#define MOD 1000000007
+#define MAX 1000
+#define f1(i,a) for(int i=0;i<a;++i)
+#define f2(i,a) for(int i=a-1;i>=0;--i)
+#define yes printf("YES\n");
+#define no printf("NO\n");
+#define Sort(a) sort(a,a+n);
+#define Reverse(a) reverse(a,a+n);
+typedef ll int lli;
+const int mxSize=2e5+10;
+using namespace std;
+
+bool com(int a,int b){
+	return a>b;
+}
+bool cmp(pair<int,int>p,pair<int,int>q){
+	return p.first>q.first;
+	return p.second>q.second;
+}
+
+ll dp[mxSize];
+bool ok=true;
+
+int solve(){
+	int a,b;
+	cin>>a>>b;
+	float x=round((a+b)/2.0);
+	
+	cout<<(int)x<<endl;
+	return 0;
+}
+
+int main(){
+	solve();
+	return 0;
+}
