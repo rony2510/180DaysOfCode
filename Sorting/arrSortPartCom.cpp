06a41365b8352a093531c36220de78ec859d28a2
rony2510 <ronyhajong2000@gmail.com>
2022-07-07 14:17:13 +0600

    day 7 Sorting

---
+++ Sorting/arrSortPartCom.cpp
@@ -0,0 +1,82 @@
+/**
+ *    author:  Captcha
+ * 	       RONY HAJONG
+ * 	       CSE'18, SUST
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool com(int a,int b){
+   return a>b;
+}
+void iniToMid(int a[],int n){
+   sort(a,a+3,com);
+   for(int i=0;i<n;++i){
+      cout<<a[i]<<" ";
+   }
+   cout<<endl;
+}
+
+void midToLast(int a[],int n){
+   sort(a+4,a+n,com);
+   for(int i=0;i<n;++i){
+      cout<<a[i]<<" ";
+   }
+   cout<<endl;
+}
+
+void midToMid(int a[],int n){
+   sort(a+4,a+7,com);
+   for(int i=0;i<n;++i){
+      cout<<a[i]<<" ";
+   }
+   cout<<endl;
+}
+int main(){
+
+   int n;
+   cin>>n;
+   int a[n];
+
+   for(int i=0;i<n;++i){
+      cin>>a[i];
+   }
+
+   for(int i=0;i<n;++i){
+      cout<<a[i]<<" ";
+   }
+   cout<<endl;
+
+
+   // iniToMid(a,n);
+   // midToLast(a,n);
+   midToMid(a,n);
+
+   
+
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+**/
