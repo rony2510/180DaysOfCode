b6560216bbb6ebfcb800bc67f89f215e8e9be3cb
rony2510 <ronyhajong2000@gmail.com>
2022-07-14 22:14:15 +0600

    day 14 recursion

---
+++ recursion/fibonacci.cpp
@@ -0,0 +1,62 @@
+/**
+ *    author:  Captcha
+ * 	       RONY HAJONG
+ * 	       CSE'18, SUST
+**/
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false);cin.tie(0);
+#define ll long long
+#define pi acos(-1)
+#define MOD 1000000007
+#define MAX 1000
+#define f1(i,a) for(int i=0;i<a;++i)
+#define f2(i,a) for(int i=a-1;i>=0;--i)
+#define yes printf("YES\n");
+#define no printf("NO\n");
+typedef ll int lli;
+const int mxSize=2e5+10;
+using namespace std;
+
+bool com(int a,int b){
+	return a>b;
+}
+bool cmp(pair<int,int>p,pair<int,int>q){
+	return p.first>q.first;
+	return p.second>q.second;
+}
+
+
+ll dp[mxSize];
+
+
+int fib(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1 || n==2){
+        return 1;
+    }
+    return fib(n-1)+fib(n-2);
+}
+int solve(){
+
+    int n;
+    cin>>n;
+    f1(i,n){
+        cout<<fib(i)<<" ";
+    }
+    cout<<"\n";
+	return 0;
+}
+
+
+
+int main(){
+	// fastio
+	int t=1;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
