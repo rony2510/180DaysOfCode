cc2e9719a92ce0fa6abae1353ccedef000c72852
rony2510 <ronyhajong2000@gmail.com>
2022-07-19 18:41:37 +0600

    day 19 c++

---
+++ C++/memset.cpp
@@ -0,0 +1,57 @@
+/**
+ *    author:  Captcha
+ * 	       RONY HAJONG
+ * 	       CSE'18, SUST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+   int n;
+   cin>>n;
+   char a[n];
+   for(int i=0;i<n;++i){
+      cin>>a[i];
+   }
+   for(int i=0;i<n;++i){
+      cout<<a[i]<<" ";
+   }
+   cout<<endl;
+
+   memset(a,'0',5);
+
+   for(int i=0;i<n;++i){
+      cout<<a[i]<<" ";
+   }
+   cout<<endl;
+   
+   
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
