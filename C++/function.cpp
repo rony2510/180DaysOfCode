c5ab1331ebc568728164be0126260a059849ffea
rony2510 <ronyhajong2000@gmail.com>
2022-07-21 22:02:25 +0600

    day 21 c++

---
+++ C++/function.cpp
@@ -0,0 +1,50 @@
+/**
+ *    author:  Captcha
+ * 	       RONY HAJONG
+ * 	       CSE'18, SUST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int add(int x,int y){
+   return x+y;
+}
+void subtract(int x,int y){
+   cout<<x-y<<endl;
+}
+int main(){
+
+   cout<<add(5,10)<<endl;
+
+   subtract(100,60);
+
+   
+   
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
