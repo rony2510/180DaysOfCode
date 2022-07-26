a6af61461bc2f151cf12bbdea41d5950449597da
rony2510 <ronyhajong2000@gmail.com>
2022-07-27 00:23:20 +0600

    day 27 c++ operator

---
+++ C++ Operator/modulo.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+10
+5 5
+0
+5 4
+1
+5 10
+5
+10 -11
+10
+10 -10
+0
+10 -6
+4
+-10 12
+-10
+-10 10
+0
+-10 8
+-2
+-10 -13
+-10
+-10 -10
+0
+-10 -4
+-2
+*/
+
+int solve(){
+
+    long x,y;
+    cin>>x>>y;
+    cout<<x%y<<endl;
+
+
+
+	return 0;
+}
+int main(){
+    int test;
+    cin>>test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
