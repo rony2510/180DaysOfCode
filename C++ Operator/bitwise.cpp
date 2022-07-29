26d1206283e13da094865afb32119fc4fb0c824f
rony2510 <ronyhajong2000@gmail.com>
2022-07-29 21:39:09 +0600

    day 29 c++ operator

---
+++ C++ Operator/bitwise.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+&
+|
+^
+~
+>>
+<<
+*/
+
+int solve(){
+
+
+
+    int x,y;
+    cin>>x>>y;
+    cout<<(x&y)<<endl;
+    cout<<(x|y)<<endl;
+    cout<<(x^y)<<endl;
+    cout<<(x>>y)<<endl;
+    cout<<(x<<y)<<endl;
+    cout<<~x<<endl;
+
+
+
+
+
+	return 0;
+}
+int main(){
+    int test;
+    cin>>test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
