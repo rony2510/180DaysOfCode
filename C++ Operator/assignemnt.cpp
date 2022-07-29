28ef5e3834a8d7da1fc7ff95f4d151b3d7505497
rony2510 <ronyhajong2000@gmail.com>
2022-07-29 21:06:38 +0600

    day 29 c++ operator

---
+++ C++ Operator/assignemnt.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int solve(){
+
+    int x;
+    x=50;
+    cout<<x<<endl;
+    x+=5;
+    cout<<x<<endl;
+    x-=3;
+    cout<<x<<endl;
+    x*=3;
+    cout<<x<<endl;
+    x/=3;
+    cout<<x<<endl;
+    x%=3;
+    cout<<x<<endl;
+    x&=5;
+    cout<<x<<endl;
+    x|=5;
+    cout<<x<<endl;
+    x^=5;
+    cout<<x<<endl;
+    x>>=5;
+    cout<<x<<endl;
+    x<<=5;
+    cout<<x<<endl;
+
+
+
+	return 0;
+}
+int main(){
+    int test;
+    cin>>test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
