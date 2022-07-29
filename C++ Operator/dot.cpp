ddaa3e3b5675049f46e41f5b53b5ba74cf9aa40c
rony2510 <ronyhajong2000@gmail.com>
2022-07-29 21:46:00 +0600

    day 29 c++ operator

---
+++ C++ Operator/dot.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+.
+*/
+
+int solve(){
+
+    vector<int>v;
+    v.push_back(100);
+    v.push_back(50);
+    v.push_back(5);
+    v.pop_back();
+
+    for(auto i:v){
+        cout<<i<<endl;
+    }
+
+
+	return 0;
+}
+int main(){
+    int test;
+    cin>>test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
