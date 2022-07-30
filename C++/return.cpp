0122319d4afaed5d2cf50c5af291189a779b489e
rony2510 <ronyhajong2000@gmail.com>
2022-07-30 20:37:43 +0600

    day 30 c++

---
+++ C++/return.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    return (5>10)?cout<<"Whilte"<<endl:cout<<"Blue"<<endl,0;
+}
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
