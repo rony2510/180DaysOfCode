8b02b78a0471231e4f1b88aeef1c818f5a6ce945
rony2510 <ronyhajong2000@gmail.com>
2022-07-30 20:49:54 +0600

    day 30 string

---
+++ String/string.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    string s;
+    cin>>s;
+    cout<<s<<endl;
+    
+}
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
