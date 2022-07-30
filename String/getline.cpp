7370b93fa6dd9ab03308b2d2e91d97b63641cb3e
rony2510 <ronyhajong2000@gmail.com>
2022-07-31 00:06:38 +0600

    day 31 String

---
+++ String/getline.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    string s;
+    int n;
+    cin>>n>>ws;
+    while(n--){
+        getline(cin,s);
+        cout<<s<<endl;
+    }
+
+}
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
