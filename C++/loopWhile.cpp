1094bf6b5a1525b93d3b187fc4e06a53ab4b7e72
rony2510 <ronyhajong2000@gmail.com>
2022-07-30 20:25:39 +0600

    day 30 c++

---
+++ C++/loopWhile.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    int x,y;
+    cin>>x>>y;
+
+    while(x<=y){
+        cout<<x<<endl;
+        ++x;
+    }
+    
+    return 0;
+}
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
