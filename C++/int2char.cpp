b3bc41e190fa96a600da7d9b332b84945a57edf9
rony2510 <ronyhajong2000@gmail.com>
2022-07-31 19:51:13 +0600

    day 31 c++

---
+++ C++/int2char.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void int2char(){
+
+    int x;
+    cin>>x;
+    char c=x+'0';
+    cout<<c<<endl;
+
+    return;
+}
+
+int main(){
+
+    int2char();
+
+    return 0;
+}
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
