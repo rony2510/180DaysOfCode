575508b83432541821d7e58ac054c227aab7a472
rony2510 <ronyhajong2000@gmail.com>
2022-08-03 20:33:30 +0600

    day 34 meme

---
+++ MeMe/!life.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    for(int i=1;i<=50;++i){
+        cout<<"i am programmer,i have !life"<<endl;
+    }
+
+    
+
+    return 0;
+}
+
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
