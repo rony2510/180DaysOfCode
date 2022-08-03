5e7f7827e732cac1def34a2c194ba064eb86b875
rony2510 <ronyhajong2000@gmail.com>
2022-08-03 20:42:14 +0600

    day 34 c++

---
+++ C++/break.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    for(int i=1;i<=50;++i){
+        if(i>=10){
+            break;
+        }
+        cout<<"break"<<endl;
+    }
+
+    
+
+    return 0;
+}
+
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
