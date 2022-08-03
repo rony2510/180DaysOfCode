ce5189802b3dc4cb5e1ef90c163db5689596298c
rony2510 <ronyhajong2000@gmail.com>
2022-08-03 20:52:55 +0600

    day 34 c++

---
+++ C++/switch.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    char c;
+    cin>>c;
+    switch (c)
+    {
+    case 'a':
+        cout<<"A a"<<endl;
+        break;
+    case 'b':
+        cout<<"B b"<<endl;
+        break;
+    case 'c':
+        cout<<"C c"<<endl;
+        break;
+    case 'd':
+        cout<<"D d"<<endl;
+        break;
+    default:
+        cout<<char(c-32)<<" "<<c<<endl;
+        break;
+    }
+
+    
+
+    return 0;
+}
+
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
