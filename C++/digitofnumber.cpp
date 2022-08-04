cea6e8f2fd6936e245ec5963a1718ec1567c97d7
rony2510 <ronyhajong2000@gmail.com>
2022-08-05 02:50:32 +0600

    day 36 c++

---
+++ C++/digitofnumber.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    int n;
+    cin>>n;
+    while(n){
+        cout<<n%10<<" ";
+        n/=10;
+    }
+    cout<<endl;
+
+
+    return 0;
+}
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
---
+++ C++/inputInfinite.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*
+to stop taking input press ctrl+Z
+*/
+int main(){
+
+/*
+    string s;
+    while(getline(cin,s)){
+        cout<<s<<endl;
+    }
+*/
+    int x;
+    while(cin>>x){
+        cout<<x<<endl;
+    }
+
+
+    return 0;
+}
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
