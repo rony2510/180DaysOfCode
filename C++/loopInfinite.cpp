755867d0e01629ba4abfad607bfbfd0eef5717d1
rony2510 <ronyhajong2000@gmail.com>
2022-08-05 02:42:16 +0600

    day 36 c++

---
+++ C++/loopInfinite.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+/*
+infinite loop
+*/
+int main(){
+    int x=0;
+    for(;;){
+        cout<<x<<" ";
+        ++x;
+        if(x==10){
+            break;
+        }
+    }
+    cout<<endl;
+
+    int y=0;
+    while(1){
+        cout<<y<<" ";
+        ++y;
+        if(y>=10){
+            break;
+        }
+    }
+    cout<<endl;
+
+    for(int i=1;;++i){
+        cout<<i<<" ";
+        if(i>=15){
+            break;
+        }
+    }
+    cout<<endl;
+
+    return 0;
+
+}
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
