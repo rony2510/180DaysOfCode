49485263db6883034b5919cfd24303b20146d333
rony2510 <ronyhajong2000@gmail.com>
2022-08-05 02:34:47 +0600

    day 36 c++

---
+++ C++/typecasting.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+   
+int main(){
+
+    int x;
+    cin>>x;
+    cout<<(long long)x*x<<endl;
+
+    double y;
+    cin>>y;
+    cout<<y<<endl<<(long long)y<<endl;
+    
+    return 0;
+}
+
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
