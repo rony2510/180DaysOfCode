1b1e553f790541a23033f88985df38c9887c5806
rony2510 <ronyhajong2000@gmail.com>
2022-08-05 02:29:29 +0600

    day 36 c++

---
+++ C++/max.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+   
+int main(){
+
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<max({a,b,c,d,e})<<endl;
+    
+    return 0;
+}
+
+
+
+
+
+/**
+ * https://codeforces.com/profile/Captcha
+ * https://www.codechef.com/users/codecloude
+ * https://atcoder.jp/users/Captcha
+ * https://github.com/rony2510
+ * https://www.linkedin.com/in/rony2510/
+ * https://www.flickr.com/people/_ronyl0__/
+**/
